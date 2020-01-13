7f444259ccc85817881f2ba2530639591edf127d
Nikita2662 <59262729+Nikita2662@users.noreply.github.com>
2020-01-13 00:25:40 -0800

    Modified code builds successfully
    Cloned and modified code from SPARK MAX Example: Read Encoder Values. Several methods from example code remain but may be unnecessary. Created two encoder objects to read encoder values. Moved robot in one of 4 directions. Used encoder object and GetVelocity() to check what direction (left or right only) the robot moved in.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -4,13 +4,18 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-#include<frc/smartdashboard/SmartDashboard.h>
+
 #include <frc/TimedRobot.h>
-#include "rev/CANSparkMax.h" 
+#include <frc/smartdashboard/SmartDashboard.h>
+#include "rev/CANSparkMax.h"
 
-
+/**
+ * Sample program displaying position and velocity on the SmartDashboard
+ * 
+ * Position is displayed in revolutions and velocity is displayed in RPM
+ */
 class Robot : public frc::TimedRobot {
-  /**
+   /**
    * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
    * 
    * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
@@ -30,11 +35,21 @@ class Robot : public frc::TimedRobot {
   rev::CANSparkMax* m_rightFollowMotor = new rev::CANSparkMax(rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
 
   /**
-   * A CANAnalog object is constructed using the GetAnalog() method on an 
-   * existing CANSparkMax object. 
+   * In order to read encoder values encoder objects are created using the 
+   * GetEncoder() method from an existing CANSparkMax object
    */
-  rev::CANAnalog m_analogSensor_left_motor = m_leftLeadMotor->GetAnalog();
-  rev::CANAnalog m_analogSensor_right_motor = m_rightLeadMotor->GetAnalog();
+
+  // CHECK FORMAT OF THESE FOLLOWING LINES: (also should these be pointers?)
+  // no encoder object created for the follow motors because they will reflect the encoders on the lead motors
+  rev::CANEncoder m_encoder_left_motor = m_leftLeadMotor->GetEncoder(); 
+  rev::CANEncoder m_encoder_right_motor = m_rightLeadMotor->GetEncoder(); 
+
+  // moves robot in one of four directions by
+  // setting speed of left and right motors
+  void Move(double speedLeft, double speedRight) {
+      m_leftLeadMotor->Set(speedLeft);
+      m_rightLeadMotor->Set(speedRight);
+    };
 
   /**
    * In RobotInit() below, we will configure m_leftFollowMotor and m_rightFollowMotor to follow 
@@ -43,91 +58,92 @@ class Robot : public frc::TimedRobot {
    * Because of this, we only need to pass the lead motors to m_robotDrive. Whatever commands are 
    * sent to them will automatically be copied by the follower motors
    */
-  
-    void Move(double speed) {
-      // set left motor forward 
-      m_leftLeadMotor->Set(speed);
-      // set right motor forward 
-      m_rightLeadMotor->Set(speed);
-    };
 
-    void TurnLeft(double speed) {
-      // setting left motor back (negative voltage)
-      m_leftLeadMotor->Set(-speed);
-      // setting right motor forward (positive voltage)
-      m_rightLeadMotor->Set(speed);
-    };
+  public:
+    void RobotInit() {
+      /**
+       * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+       * in the SPARK MAX to their factory default state. If no argument is passed, these
+       * parameters will not persist between power cycles
+       */
 
-    void TurnRight(double speed) {
-      // setting left motor forward (positive voltage)
-      m_leftLeadMotor->Set(speed);
-      // setting right motor back (negative voltage)
-      m_rightLeadMotor->Set(-speed);
-    };
-
- public:
-  void RobotInit() {
-    /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
-     */
-    m_leftLeadMotor->RestoreFactoryDefaults();
-    m_rightLeadMotor->RestoreFactoryDefaults();
-    m_leftFollowMotor->RestoreFactoryDefaults();
-    m_rightFollowMotor->RestoreFactoryDefaults();
+      m_leftLeadMotor->RestoreFactoryDefaults();
+      m_rightLeadMotor->RestoreFactoryDefaults();
+      m_leftFollowMotor->RestoreFactoryDefaults();
+      m_rightFollowMotor->RestoreFactoryDefaults();
     
-    /**
-     * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
-     * the Follow() method on the SPARK MAX you want to configure as a follower, and by passing
-     * as a parameter the SPARK MAX you want to configure as a leader.
-     * 
-     * This is shown in the example below, where one motor on each side of our drive train is
-     * configured to follow a lead motor.
-     */
-    m_leftFollowMotor->Follow(*m_leftLeadMotor);
-    m_rightFollowMotor->Follow(*m_rightLeadMotor);
+      /**
+       * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
+       * the Follow() method on the SPARK MAX you want to configure as a follower, and by passing
+       * as a parameter the SPARK MAX you want to configure as a leader.
+       *    
+       * This is shown in the example below, where one motor on each side of our drive train is
+       * configured to follow a lead motor.
+       */
+      m_leftFollowMotor->Follow(*m_leftLeadMotor);
+      m_rightFollowMotor->Follow(*m_rightLeadMotor);
   };
 
-  void TeleopPeriodic() {
-    //setting the speed to 1 so that robot moves forward
-    Move(1);
-    // read the voltage on the lead motors to check if motors are moving forward 
-    if((m_analogSensor_left_motor.GetVoltage() > 0) && (m_analogSensor_right_motor.GetVoltage() > 0)) {
-      frc::SmartDashboard::PutString("motor Sensor Positive voltage", "Motor moving forward");
-    } else { // error message
-      frc::SmartDashboard::PutString("ERROR:Motor Sensor voltage", "Not positive");
+
+  // IS THIS NECESSARY?
+  public:
+   Robot() { }
+
+  void TeleopPeriodic() override {
+    /**
+     * Encoder position is read from a CANEncoder object by calling the
+     * GetPosition() method.
+     * 
+     * GetPosition() returns the position of the encoder in units of revolutions
+     */
+
+    // IS THIS NECESSARY?
+    frc::SmartDashboard::PutNumber("Left Encoder Position", m_encoder_left_motor.GetPosition());
+    frc::SmartDashboard::PutNumber("Right Encoder Position", m_encoder_right_motor.GetPosition());
+
+    /**
+     * Encoder velocity is read from a CANEncoder object by calling the
+     * GetVelocity() method.
+     * 
+     * GetVelocity() returns the velocity of the encoder in units of RPM
+     */
+
+    // IS THIS NECESSARY?
+    frc::SmartDashboard::PutNumber("Left Encoder Velocity", m_encoder_left_motor.GetVelocity());
+    frc::SmartDashboard::PutNumber("Right Encoder Velocity", m_encoder_right_motor.GetVelocity());
+
+    /**
+     * moves the robot in one of 4 directions
+     * also checks if the range for the speed of the motors is between -0.5 and 0.5 (half speed)
+     * if the robot is moving left, checks that the speed of the left motor < speed of right motor
+     * if the robot is moving right, checks that the speed of the right motor < speed of left motor
+     * Note: with a dynamic input, 3 ways to move left (and similarly for right):
+          * left motor: negative speed, right motor: positive speed
+          * left motor: relatively slower, right motor: relatively faster
+          * left motor: speed = 0; right motor: positive speed
+     */
+    Move (0.5, 0.5); // forward
+    Move (-0.5, -0.5); // backward
+    Move (0, 0.5); // left
+    Move (0.5, 0); // right
+
+    /**
+     * uses the encoders and GetVelocity()
+     * to check in what direction the robot moves 
+     */
+
+    // if speed of left motor < speed of right, robot moving left
+    if (m_encoder_left_motor.GetVelocity() < m_encoder_right_motor.GetVelocity()) {
+      // STORE IN FILE
+    }
+
+    // if speed of left motor > speed of right, robot moving right
+    else if (m_encoder_left_motor.GetVelocity() > m_encoder_right_motor.GetVelocity()) {
+      // STORE IN FILE
     };
 
-    //setting the speed to -1 so that robot moves backward
-    Move(-1);
-    // read the voltage on the lead motors to check if motors are moving backward
-    if((m_analogSensor_left_motor.GetVoltage() < 0) && (m_analogSensor_right_motor.GetVoltage() < 0)) {
-      frc::SmartDashboard::PutString("Motor Sensor Negative voltage", "Motor moving backward");
-    } else { // error message
-      frc::SmartDashboard::PutString("ERROR:Motor Sensor voltage", "Not negative");
-    };
-
-    // moving left
-    TurnLeft(1);
-    // read the voltage on the lead motors to check if robot is moving left
-    if((m_analogSensor_left_motor.GetVoltage() < 0) && (m_analogSensor_right_motor.GetVoltage() > 0)) {
-      frc::SmartDashboard::PutString("Left - Negative Voltage; Right - Positive Voltage", "Motor moving left");
-    } else { // error message
-      frc::SmartDashboard::PutString("ERROR:Motor Sensor voltage", "Motor not moving left");
-    };
-
-    // turning right
-    TurnRight(1);
-    // read the voltage on the lead motors to check if robot is moving right
-    if((m_analogSensor_left_motor.GetVoltage() > 0) && (m_analogSensor_right_motor.GetVoltage() < 0)) {
-      frc::SmartDashboard::PutString("Left - Positive Voltage; Right - Negative Voltage", "Motor moving right");
-    } else { // error message
-      frc::SmartDashboard::PutString("ERROR:Motor Sensor voltage", "Motor not moving right");
-    };
-    
-    // setting speed to 0 to stop the motor
-    Move(0);
+    // moving forward
+    // moving backward
   };
 };
 
